ac713ede0609d94ee318b27b21ca4cc7433acd99
pramk7 <parshuram_pm777@hotmail.com>
2021-05-10 12:22:36 +0530

    first commit
    files uploaded

---
+++ src/test/java/com/jdbctojpa/In28MinJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdbctojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class In28MinJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
