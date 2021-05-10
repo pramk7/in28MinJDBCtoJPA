fea9066126069802241dce42c91713ab118e0648
pramk7 <parshuram.kadam@jaymaheshautomall.in>
2021-05-10 12:28:40 +0530

    first commit from eclipse

---
+++ src/main/java/com/jdbctojpa/In28MinJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.jdbctojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class In28MinJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(In28MinJdbcApplication.class, args);
+	}
+
+}
